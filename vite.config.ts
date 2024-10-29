a69984242b56ff7bb227b727870c1e582f4e9727
Simon <SimonVutov1@gmail.com>
2024-10-28 21:09:11 -0400

    Initial Commit
    Initial Commit

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: '/lifeai/', // Replace <your-repo-name> with the actual repository name
-  plugins: [react()],
-});
